cc921f8904299d67c31e611f5e9f817ae61a1eef
francis-hrom <74234592+francis-hrom@users.noreply.github.com>
2021-09-01 09:31:38 +0200

    added explainer image

--- src/pages/FindSelectorPage/FindSelectorPage.tsx
+++ src/pages/FindSelectorPage/FindSelectorPage.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import FindSelector from "../../components/FindSelector/FindSelector";
 import Card from "@material-ui/core/Card";
 import Container from "@material-ui/core/Container";
+import { Row, Col, Image } from "react-bootstrap";
 
 const FindSelectorPage = (): JSX.Element => (
   <div className="FindSelectorPage" data-testid="FindSelectorPage">
     <Card>
       <Container>
-        <FindSelector />
+        <Row>
+          <Col>
+            <FindSelector />
+          </Col>
+          <Col lg={6}>
+            <Image src="images/explainer.jpg" alt="explainer" fluid />
+          </Col>
+        </Row>
       </Container>
     </Card>
   </div>
--- src/pages/GetRankingsPage/GetRankingsPage.tsx
+++ src/pages/GetRankingsPage/GetRankingsPage.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import GetRankings from "../../components/GetRankings/GetRankings";
 import Card from "@material-ui/core/Card";
 import Container from "@material-ui/core/Container";
+import { Row, Col, Image } from "react-bootstrap";
 
-const GetRankingsPage = (props: any): JSX.Element => (
+const GetRankingsPage = (): JSX.Element => (
   <div className="GetRankingsPage" data-testid="GetRankingsPage">
     <Card>
       <Container>
-        <GetRankings props={props} />
+        <Row>
+          <Col>
+            <GetRankings />
+          </Col>
+          <Col lg={6}>
+            <Image src="images/explainer.jpg" alt="explainer" fluid />
+          </Col>
+        </Row>
       </Container>
     </Card>
   </div>
